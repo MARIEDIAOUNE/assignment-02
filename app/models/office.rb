dbbf7aeb2ee92970950e6dd2f5ea29ad8277e316
MARIEDIAOUNE <marie.diaoune@gmail.com>
2021-09-23 14:28:28 -0600

    Project finally works based on project requirements

--- app/models/office.rb
+++ app/models/office.rb
@@ -2,5 +2,6 @@ class Office < ApplicationRecord
     has_one :teacher, dependent: :nullify
 
     def office_BuldingAndNum
-        [building_name, room_name].join(' ')
+        [building_name, room_number].join('')
+    end
 end
