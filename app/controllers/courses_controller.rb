2ae7b49ddbe20a78451a37ba64bf335d3e4954e7
MARIEDIAOUNE <marie.diaoune@gmail.com>
2021-10-07 13:40:20 -0600

    Added correct content to show pages of sections and courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -17,6 +17,8 @@ class CoursesController < ApplicationController
 
   # GET /courses/1/edit
   def edit
+    # @course = Course.all
+    # @sections = Section.all
   end
 
   # POST /courses or /courses.json
@@ -64,6 +66,6 @@ class CoursesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def course_params
-      params.require(:course).permit(:prefix, :number, :description)
+      params.require(:course).permit(:prefix, :number, :description, section_id: [])
     end
 end
